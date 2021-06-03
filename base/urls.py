444469541785fda58944dd925581cc349e8ea05e
Shubhank-S <shubhanksharma345@gmail.com>
2021-06-03 18:00:05 +0530

    Deploy

---
+++ base/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('<str:desc>/<int:qty>/<str:price>/<int:country>', views.recommend, name='recommend'),
+    path('homepage/', views.homepage, name='homepage')
+]
